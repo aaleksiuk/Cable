b16d1b332eaa929314a6c4c48e9d051158433541
Anna Aleksiuk <aal@scalepoint.com>
2023-07-20 12:42:33 +0200

    Add project files.

---
+++ Cable/Cable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cables;
+
+public class Cable  
+{
+    string Type;
+    public Cable (string type)
+    {
+        Type = type;
+    }
+    public void Print()
+    {
+        Console.WriteLine($"Jest to kabel typu: {Type}");
+    }
+}
---
+++ Cable/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Cables;
+
+Cable cable1 = new("USB A");
+Cable cable2 = new("USB B");
+Cable cable3 = new("USB 3.0");
+Cable cable4 = new("USB mini");
+Cable cable5 = new("USB micro");
+
+List<Cable> cables = new ();
+cables.Add(cable1);
+cables.Add(cable2);
+cables.Add(cable3);
+cables.Add(cable4);
+cables.Add(cable5);
+
+foreach (var cable in cables)
+{ 
+    cable.Print();
+}
+
+Console.WriteLine($"============\nLista zawiera: {cables.Count} elementów");
